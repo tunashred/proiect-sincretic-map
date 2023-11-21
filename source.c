4bf923a6b4ee67ed1e98302e4bdfe60d37d6c6e4
tunashred <enachealexandru.traian@gmail.com>
2023-11-21 13:07:04 +0200

    Am implementat problema turnurilor din hanoi recursiv

--- source.c
+++ source.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 
-void hanoi(int disks, char initial, char aux, char final) {
-    
+void hanoi(int nr_discuri_pe_sursa, char sursa, char destinatie, char intermediar) {
+    if(nr_discuri_pe_sursa == 0) {
+        return;
+    }
+
+    hanoi(nr_discuri_pe_sursa - 1, sursa, intermediar, destinatie);
+
+    printf("Muta discul #%d de pe '%c' pe '%c'.\n", nr_discuri_pe_sursa, sursa, destinatie);
+
+    hanoi(nr_discuri_pe_sursa - 1, intermediar, destinatie, sursa);
 }
 
 int main() {
-    hanoi(10, "A", "B", "C");
+    hanoi(2, 'A', 'B', 'C');
 
     return 0;
 }
